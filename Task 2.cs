958737233eef3a2ecb4757b02a2f81075164e8c6
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-01-25 14:50:46 -0500

    Task 2 and Task 3

--- Task 2.cs
+++ Task 2.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Task2 : MonoBehaviour
 {
+
+    public int bookPrice;
+    public int copiesSold;
+
+    private float firstShippingCost = 3;
+    private float secondShippingCost = .75f;
+
+    private float discout = .4f;
+
+    private float totalCost;
+    private float totalProfit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < copiesSold; i++) 
+        {
+            if (i == 0)
+            {
+                float bookTotalCost = bookPrice * (1 - discout) + firstShippingCost;
+                totalCost += bookTotalCost;
+                totalProfit += bookPrice - bookTotalCost;
+            }
+            else
+            {
+                float bookTotalCost = bookPrice * (1 - discout) + secondShippingCost;
+                totalCost += bookTotalCost;
+                totalProfit += bookPrice - bookTotalCost;
+            }
+        }
+
+        Debug.Log($"Task 2: Total Cost: {totalCost}. Total Profit: {totalProfit}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Task 3.cs
+++ Task 3.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Task3 : MonoBehaviour
 {
+
+    public int money;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int hundreds;
+        int fifties;
+        int twenties;
+        int tens;
+        int fives;
+        int ones;
+
+        hundreds = money / 100;
+        money = money % 100;
+        fifties = money / 50;
+        money = money % 50;
+        twenties = money / 20;
+        money = money % 20;
+        tens = money / 10;
+        money = money % 10;
+        fives = money / 5;
+        money = money % 5;
+        ones = money / 1;
+        money = money % 1;
+
+        Debug.Log($"Task 3: {hundreds} 100s. {fifties} 50s. {twenties} 20s. {tens} 10s. {fives} 5s. {ones} 1s.");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
